bc63f7a83f632515723fcceed3227df22527b67b
K07-2p0 <adrianofsoliveira@gmail.com>
2025-11-29 17:32:08 +0000

    adicao api

--- src/bot/Strategy.java
+++ src/bot/Strategy.java
@@ -2,19 +2,7 @@ package src.bot;
 
 import src.model.map.Room;
 
-/**
- * Interface que define o comportamento de uma estratégia de AI para jogadores Bot.
- * As diferentes classes que implementarem esta interface (e.g., RandomStrategy, SimpleHeuristicStrategy)
- * vão ditar como o bot escolhe o seu próximo movimento.
- */
 public interface Strategy {
 
-    /**
-     * Determina a próxima sala para onde o bot deve se mover.
-     *
-     * @param currentRoom A sala atual onde o bot se encontra.
-     * @param availableRooms A lista (ou estrutura de dados personalizada) de salas adjacentes.
-     * @return A sala escolhida para o próximo movimento.
-     */
     Room determineMove(Room currentRoom, Object availableRooms);
 }
